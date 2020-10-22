f586dc98f16cdb6b0ca65e3af73c54057cf4f1f0
Abhilash Negi <tiger.abhi.3609@gmail.com>
2020-10-22 17:07:26 +0530

    Updates

---
+++ precache-manifest.6fd052c1d8cb0112c7996e96d0194196.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a4395aca0d8b4353ceb84de8caa7bb",
+    "url": "/randomJokes/index.html"
+  },
+  {
+    "revision": "c454bcfd5f21f59a72dd",
+    "url": "/randomJokes/static/css/main.bad24d1e.chunk.css"
+  },
+  {
+    "revision": "3982f821570086e40b30",
+    "url": "/randomJokes/static/js/2.695d9207.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/randomJokes/static/js/2.695d9207.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c454bcfd5f21f59a72dd",
+    "url": "/randomJokes/static/js/main.bbaa0707.chunk.js"
+  },
+  {
+    "revision": "e41e83ed241184217216",
+    "url": "/randomJokes/static/js/runtime-main.7664a35f.js"
+  }
+]);
